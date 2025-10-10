f984df06ec01b41fed5f8d4be59a2cbb62b97567
Ange Meli <kindson17@gmail.com>
2025-10-10 19:40:43 +0100

    updates

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Candidat {
+    int id;
+    char nom[500];
+    int age;
+    char parti[20];
+};
+
+struct Electeur {
+    int id;
+    char nom[500];
+    int age;
+    char quartier[20];
+};
+
+int main(int argc, char const *argv[]) {
+    /* code */
+    return 0;
+}
