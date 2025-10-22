6fb3ff46b48d0b22efd99dff6d4356916f19ee2c
ZIDANE PADA <zidanenguela8@gmail.com>
2025-10-22 10:16:32 +0100

    Update phase2.c

--- ressources/phase2.c
+++ ressources/phase2.c
@@ -4,9 +4,21 @@
 
 #include "ressources.h"
 
+// Vérifie si le parti est unique parmi les autres candidats
+int partiUnique(Candidat candidats[], int index, int total) {
+    for (int i = 0; i < total; i++) {
+        if (i != index && strcmp(candidats[i].parti, candidats[index].parti) == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+
 void verifierCandidatures(Candidat candidat[], int n){
     for(int i = 0; i < n; i++){
-        if(candidats[i].age >= 35 && candidats[i].cautionPayee == 30000000 && candidats[i].dossierComplet == 1){
+        if(candidats[i].age >= 35 && candidats[i].cautionPayee == 30000000 && partiUnique(candidats, i, n)){
             candidats[i].estValide = 1;
         }else{
             candidats[i].estValide = 0;
@@ -14,10 +26,11 @@ void verifierCandidatures(Candidat candidat[], int n){
     }
 }
 
-void afficherResultats(candidat candidats[], int n){
-    printf("\n Résultats de la vérification des candidatures :\n");
-    printf("id\tnom\tparti\tStatut\n");
-    for(int i = 0; i < n; i++){
-        printf("%d\t%-10s\t%-10s\t%s\n", candidats[i].id, candidats[i].nom, candidats[i].parti, candidats[i].estValide ? "Acceptée" : "Rejetée");
+void afficherResultats(Candidat candidats[], int n) {
+    printf("\nRésultats de la vérification des candidatures :\n");
+    printf("ID\tNom\t\tParti\t\tStatut\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%-10s\t%-10s\t%s\n", candidats[i].id, candidats[i].nom, candidats[i].parti,
+               candidats[i].estValide ? "Acceptée" : "Rejetée");
     }
 }
