d1a49a0e015f5914cd43555071ae3b5cdb82ac74
ZIDANE PADA <zidanenguela8@gmail.com>
2025-10-22 12:08:48 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "ressources/ressources.h"
+#include "ressources.h"
 
 // Fonction principale
 int main(int argc, char const *argv[]) {
@@ -183,3 +183,4 @@ int main(int argc, char const *argv[]) {
 
     return 0;
 }
+
