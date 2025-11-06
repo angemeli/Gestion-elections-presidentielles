7f38dc92d62be0f77c5a91f406418b607ea3208b
ZIDANE PADA <zidanenguela8@gmail.com>
2025-11-06 09:14:30 +0100

    Update phase2.c

--- ressources/phase2.c
+++ ressources/phase2.c
@@ -4,33 +4,37 @@
 
 #include "ressources.h"
 
-int partiUnique(Candidat candidat[], int index, int total) {
+// Vérifie si le parti est unique parmi les autres candidats
+int partiUnique() {
+    int index, total;
     for (int i = 0; i < total; i++) {
-        if (i != index && strcmp(candidat[i].parti, candidat[index].parti) == 0) {
+        if (i != index && strcmp(liste_candidats[i].parti, liste_candidats[index].parti) == 0) {
             return 0;
         }
     }
     return 1;
 }
 
-void afficherResultats(Candidat candidat[], int n) {
-    printf("\nResultats de la verification des candidatures :\n");
-    for (int i = 0; i < n; i++) {
-        Candidat c = candidat[i];
-        printf("\n%d. %s (%s) : %s", c.id, c.nom, c.parti,
-            c.estValide == 1 ? "Acceptee" : "Rejetee");
-    }
-    printf("\n");
-}
 
-void verifierCandidatures(Candidat candidat[], int n) {
+
+void verifierCandidatures(){
+    int n;
     for(int i = 0; i < n; i++){
-        if(candidat[i].age >= 35 && candidat[i].caution == 30000000 && partiUnique(candidat, i, n)){
-            candidat[i].estValide = 1;
-        } else{
-            candidat[i].estValide = 0;
+        if(liste_candidats[i].age >= 35 && liste_candidats[i].caution == 30000000 && partiUnique()){
+            liste_candidats[i].estValide = 1;
+        }else{
+            liste_candidats[i].estValide = 0;
         }
     }
+    printf("Evaluation términée.");
+}
 
-    afficherResultats(candidat, n);
+void afficherResultats() {
+    int n;
+    printf("\nRésultats de la vérification des candidatures :\n");
+    printf("ID\tNom\t\tParti\t\tStatut\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%-10s\t%-10s\t%s\n", liste_candidats[i].id, liste_candidats[i].nom, liste_candidats[i].parti,
+               liste_candidats[i].estValide ? "Acceptée" : "Rejetée");
+    }
 }
